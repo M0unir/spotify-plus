fbca68332c046cd5cffb7c7131ca8aafdc15b98d
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-06-29 17:27:15 -0700

    Added nodemon start script

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Spotify Plus')
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
