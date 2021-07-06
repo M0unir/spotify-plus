dab7f5246238178d4731a662e4fd0aba2c9df1a3
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-07-06 14:30:46 -0700

    Grab access & refresh tokens from url

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token')
+    const refreshToken = urlParams.get('refresh_token')
+    // console.log({ accessToken, refreshToken })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
