6aee5cd0cfa9b84f1bbf5acf6d4a9cc8492732fa
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-07-07 14:51:12 -0700

    Adding a react proxy to handle CORS (hardcoded)

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,14 @@ function App() {
     const accessToken = urlParams.get('access_token')
     const refreshToken = urlParams.get('refresh_token')
     // console.log({ accessToken, refreshToken })
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
+    }
+
   }, [])
 
   return (
