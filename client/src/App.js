8af3bea12176989b7e9ec8196e068ca4aeeb684d
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-07-10 23:41:15 -0700

    Exporting spotify tokens as a service

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,32 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { spotifyService } from './spotifyService';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+
 
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token')
-    const refreshToken = urlParams.get('refresh_token')
-    // console.log({ accessToken, refreshToken })
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err))
-    }
-
+    setToken(spotifyService.accessToken);
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8080/login"
-          rel="noopener noreferrer"
-        >
-          Login to Spotify
-        </a>
+        {!token ? (
+          <a
+            className="App-link"
+            href="http://localhost:8080/login"
+            rel="noopener noreferrer"
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <h2>Logged In</h2>
+        )
+        }
       </header>
     </div>
   );
