64caae1c1841b9fc3c63c7fc33eb238e54a1e673
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-08-01 19:56:33 -0700

    Setting up Styled Components & Tooling

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,17 @@ import { toast, ToastContainer } from 'react-toastify';
 import { Route, Switch } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 // import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components/macro';
+
+const StyledLoginButton = styled.a`
+  background-color: green;
+  color: white;
+  border: 1px solid green;
+  border-radius: 10px;
+  padding: 5px 15px;
+  text-align: center;
+  display: inline-block;
+`
 
 function App() {
   // const { accessToken, logout, getUserProfile } = spotifyService;
@@ -31,53 +41,55 @@ function App() {
     <div className="App">
       <ToastContainer />
       <header className="App-header">
-        {!token ? (
-          <a
+      </header>
+      {!token ? (
+        <div className="main" style={{ display: 'block', padding: '0 10px', margin: '10px 0' }}>
+          <StyledLoginButton
             className="App-link"
             href="http://localhost:8080/login"
             rel="noopener noreferrer"
           >
             Login to Spotify
-          </a>
-        ) : (
-          <>
-            <Switch>
-              <Route path="/top-artists" component={(props) => props.match.params}>
-                <h1>Top Artists</h1>
-                {
-                  console.log('props: ',)
-                }
-              </Route>
-              <Route path="/top-tracks">
-                <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist </h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
-              </Route>
-              <Route path="/" >
-                <>
-                  <h2>Logged In</h2>
-                  <button onClick={logout}>Logout</button>
-                  <br />
-                  {profile && (
-                    <>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="User Profile" />
-                      )}
-                      <p>Hello {profile.display_name}, email: {profile.email}</p>
-                    </>
+          </StyledLoginButton>
+        </div>
+      ) : (
+        <>
+          <Switch>
+            <Route path="/top-artists" component={(props) => props.match.params}>
+              <h1>Top Artists</h1>
+              {
+                console.log('props: ',)
+              }
+            </Route>
+            <Route path="/top-tracks">
+              <h1>Top Tracks</h1>
+            </Route>
+            <Route path="/playlists/:id">
+              <h1>Playlist </h1>
+            </Route>
+            <Route path="/playlists">
+              <h1>Playlists</h1>
+            </Route>
+            <Route path="/" >
+              <>
+                <h2>Logged In</h2>
+                <button onClick={logout}>Logout</button>
+                <br />
+                {profile && (
+                  <>
+                    {profile.images.length && profile.images[0].url && (
+                      <img src={profile.images[0].url} alt="User Profile" />
+                    )}
+                    <p>Hello {profile.display_name}, email: {profile.email}</p>
+                  </>
 
-                  )}
-                </>
-              </Route>
-            </Switch>
-          </>
-        )
-        }
-      </header>
+                )}
+              </>
+            </Route>
+          </Switch>
+        </>
+      )
+      }
     </div>
   );
 }
