7c2416c15c32a69ee272fe5f765d62202d8cfa40
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-08-07 23:01:16 -0700

    Added StyledHeader styled component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getUserProfile } from '../services/spotifyService';
 import { toast } from 'react-toastify';
+import { StyledHeader } from '../styles/'
 
 const Profile = () => {
     const [profile, setProfile] = useState(null);
@@ -25,10 +26,22 @@ const Profile = () => {
         <>
             {profile && (
                 <>
-                    {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="User Profile" />
-                    )}
-                    <p>Hello {profile.display_name}, email: {profile.email}</p>
+                    <StyledHeader type="user">
+                        <div className="header__inner">
+                            {profile.images.length && profile.images[0].url && (
+                                <img className="header__img" src={profile.images[0].url} alt="Avatar" />
+                            )}
+                            <div>
+                                <div className="header__overline">Profile</div>
+                                <h1 className="header__name">{profile.display_name}</h1>
+                                <p className="header__meta">
+                                    <span>
+                                        {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </StyledHeader>
                 </>
 
             )}
--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,1 +1,2 @@
 export { default as GlobalStyles } from './GlobalStyles'
+export { default as StyledHeader } from './StyledHeader'
