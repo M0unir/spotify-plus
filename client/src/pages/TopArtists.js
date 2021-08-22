0cf14c9af130a564cfec7f6707b5af63fc48fb56
Mounir Chouladi <mounir.chouladi@gmail.com>
2021-08-22 12:30:26 -0700

    Added Filter Top Artists by Time Range

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -5,19 +5,26 @@ import { getUserTopArtists } from '../services/spotifyService'
 const TopArtists = () => {
 
     const [topArtists, setTopArtists] = useState(null);
+    const [activeTimeRange, setActiveTimeRange] = useState('long_term');
 
     useEffect(() => {
         const getTopArtists = async () => {
-            const { data } = await getUserTopArtists();
+            const { data } = await getUserTopArtists(activeTimeRange);
             setTopArtists(data)
         }
         getTopArtists();
-    }, [])
+    }, [activeTimeRange])
 
     return (
         <main>
+
             {topArtists && (
-                <Section title="Top artists this month" breadcrumb="true">
+                <Section title="Top artists" breadcrumb="true">
+                    <ul>
+                        <li><button onClick={() => setActiveTimeRange('short_term')}>Last month</button></li>
+                        <li><button onClick={() => setActiveTimeRange('medium_term')}>Last 6 months</button></li>
+                        <li><button onClick={() => setActiveTimeRange('long_term')}>All time</button></li>
+                    </ul>
                     <Artists artists={topArtists.items.slice(0, 15)} />
                 </Section>
             )
